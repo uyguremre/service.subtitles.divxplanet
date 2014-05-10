a4f1592967f865a0394e9b10e2139f6d505eefc9
Koray Al <koray.al@vendeka.com.tr>
2014-05-11 00:34:25 +0300

    - fixed empty year search on tv shows - fixed empty result crash - normalized saved filenames - cleaned up text in parantheses in tv show titles

--- service.subtitles.divxplanet/service.py
+++ service.subtitles.divxplanet/service.py
@@ -30,8 +30,13 @@ __temp__       = xbmc.translatePath( os.path.join( __profile__, 'temp') ).decode
 
 sys.path.append (__resource__)
 
+
 def getmediaUrl(mediaArgs):
-    query = "site:divxplanet.com inurl:sub/m \"%s ekibi\" intitle:\"%s\" intitle:\"(%s)\"" % (mediaArgs[0], mediaArgs[1], mediaArgs[2])
+    title = re.sub(" \(?(.*.)\)", "", mediaArgs[1])
+    if mediaArgs[2] != "":
+      query = "site:divxplanet.com inurl:sub/m \"%s ekibi\" intitle:\"%s\" intitle:\"(%s)\"" % (mediaArgs[0], title, mediaArgs[2])
+    else:
+      query = "site:divxplanet.com inurl:sub/m \"%s ekibi\" intitle:\"%s\"" % (mediaArgs[0], title)
     br = mechanize.Browser()
     log("Divxplanet: Finding media %s" % query)
     # Cookie Jar
@@ -68,9 +73,11 @@ def getmediaUrl(mediaArgs):
             linkurl = re.search(r"\/url\?q=(http:\/\/divxplanet.com\/sub\/m\/[0-9]{3,8}\/.*.\.html).*", sLink["href"])
             if linkurl:
                 linkdictionary.append({"text": sSpan.getText().encode('ascii', 'ignore'), "name": mediaArgs[0], "url": linkurl.group(1)})
-    log("Divxplanet: found media: %s" % (linkdictionary[0]["url"]))
-    return linkdictionary[0]["url"]
-""
+                log("Divxplanet: found media: %s" % (linkdictionary[0]["url"]))
+    if len(linkdictionary) > 0:
+      return linkdictionary[0]["url"]
+    else:
+      return ""
 
 def Search(item):
 #def search_subtitles(tvshow, year, season, episode, title): #standard input
@@ -85,68 +92,69 @@ def Search(item):
         log("Divxplanet: searching subtitles for %s %s %s %s" % (tvshow, year, season, episode))
         tvurl = getmediaUrl(["dizi",tvshow, year])
         log("Divxplanet: got media url %s" % (tvurl))
-        divpname = re.search(r"http:\/\/divxplanet.com\/sub\/m\/[0-9]{3,8}\/(.*.)\.html", tvurl).group(1)
-        season = int(season)
-        episode = int(episode)
-        # Browser
-        br = mechanize.Browser()
+        if tvurl != "":
+          divpname = re.search(r"http:\/\/divxplanet.com\/sub\/m\/[0-9]{3,8}\/(.*.)\.html", tvurl).group(1)
+          season = int(season)
+          episode = int(episode)
+          # Browser
+          br = mechanize.Browser()
 
-        # Cookie Jar
-        cj = cookielib.LWPCookieJar()
-        br.set_cookiejar(cj)
+          # Cookie Jar
+          cj = cookielib.LWPCookieJar()
+          br.set_cookiejar(cj)
 
-        # Browser options
-        br.set_handle_equiv(True)
-        # br.set_handle_gzip(True)
-        br.set_handle_redirect(True)
-        br.set_handle_referer(True)
-        br.set_handle_robots(False)
+          # Browser options
+          br.set_handle_equiv(True)
+          # br.set_handle_gzip(True)
+          br.set_handle_redirect(True)
+          br.set_handle_referer(True)
+          br.set_handle_robots(False)
 
-        # Follows refresh 0 but not hangs on refresh > 0
-        br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+          # Follows refresh 0 but not hangs on refresh > 0
+          br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-        # User-Agent (this is cheating, ok?)
-        br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+          # User-Agent (this is cheating, ok?)
+          br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-        url = br.open(tvurl)
-        html = url.read()
-        soup = BeautifulSoup(html)
-        subtitles_list = []
-        i = 0
-        # /sub/s/281212/Hannibal.html
-        for link in soup.findAll('a', href=re.compile("\/sub\/s\/.*.\/%s.html" % divpname)):
-            addr = link.get('href')
-            info = link.parent.parent.nextSibling.nextSibling.findAll("td", colspan="3")
-            if info:
-                tse = info[0].div.findAll("b", text="%d" % season)
-                tep = info[0].div.findAll("b", text="%02d" % episode)
-                lantext = link.parent.find("br")
-                lan = link.parent.parent.findAll("img", title=re.compile("^.*. (subtitle|altyazi)"))
-                if tse and tep and lan and lantext:
-                    language = lan[0]["title"]
-                    filename = info[1].getText()
-                    if language[0] == "e":
-                        language = "English"
-                        lan_short = "en"
-                    else:
-                        language = "Turkish"
-                        lan_short = "tr"
-                    filename = "%s S%02dE%02d %s.%s" % (tvshow, season, episode, title, lan_short)
-                    description = info[1].getText()
-                    listitem = xbmcgui.ListItem(label=language,                                   # language name for the found subtitle
-                              label2=description,               # file name for the found subtitle
-                              iconImage="0",                                     # rating for the subtitle, string 0-5
-                              thumbnailImage=xbmc.convertLanguage(language, xbmc.ISO_639_1)
-                              )
-                    listitem.setProperty( "hearing_imp", '{0}'.format("false").lower() ) # set to "true" if subtitle is for hearing impared
-                    subtitles_list.append(listitem)
-                    url = "plugin://%s/?action=download&link=%s&lang=%s&description=%s" % (__scriptid__,
-                                                                    addr,
-                                                                    lan_short,
-                                                                    description)
-                    xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=listitem,isFolder=False)
-        br.close()
-        log("Divxplanet: found %d subtitles" % (len(subtitles_list)))
+          url = br.open(tvurl)
+          html = url.read()
+          soup = BeautifulSoup(html)
+          subtitles_list = []
+          i = 0
+          # /sub/s/281212/Hannibal.html
+          for link in soup.findAll('a', href=re.compile("\/sub\/s\/.*.\/%s.html" % divpname)):
+              addr = link.get('href')
+              info = link.parent.parent.nextSibling.nextSibling.findAll("td", colspan="3")
+              if info:
+                  tse = info[0].div.findAll("b", text="%d" % season)
+                  tep = info[0].div.findAll("b", text="%02d" % episode)
+                  lantext = link.parent.find("br")
+                  lan = link.parent.parent.findAll("img", title=re.compile("^.*. (subtitle|altyazi)"))
+                  if tse and tep and lan and lantext:
+                      language = lan[0]["title"]
+                      filename = info[1].getText()
+                      if language[0] == "e":
+                          language = "English"
+                          lan_short = "en"
+                      else:
+                          language = "Turkish"
+                          lan_short = "tr"
+                      filename = "%s S%02dE%02d %s.%s" % (tvshow, season, episode, title, lan_short)
+                      description = info[1].getText()
+                      listitem = xbmcgui.ListItem(label=language,                                   # language name for the found subtitle
+                                label2=description,               # file name for the found subtitle
+                                iconImage="0",                                     # rating for the subtitle, string 0-5
+                                thumbnailImage=xbmc.convertLanguage(language, xbmc.ISO_639_1)
+                                )
+                      listitem.setProperty( "hearing_imp", '{0}'.format("false").lower() ) # set to "true" if subtitle is for hearing impared
+                      subtitles_list.append(listitem)
+                      url = "plugin://%s/?action=download&link=%s&lang=%s&description=%s" % (__scriptid__,
+                                                                      addr,
+                                                                      lan_short,
+                                                                      description)
+                      xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=listitem,isFolder=False)
+          br.close()
+          log("Divxplanet: found %d subtitles" % (len(subtitles_list)))
     else:
         log("Divxplanet: searching subtitles for %s %s" % (title, year))
         tvurl = getmediaUrl(["film", title, year])
@@ -251,8 +259,8 @@ def Download(link, lang, description): #standard input
     br.select_form(name="dlform")
     br.submit()
 
-    log("Divxplanet: Fetching subtitles using url '%s" % (dlurl))
-    local_tmp_file = os.path.join(__temp__, description + ".rar")
+    log("Divxplanet: Fetching subtitles using url %s" % (dlurl))
+    local_tmp_file = os.path.join(__temp__, normalizeString(description) + ".rar")
     try:
         log("Divxplanet: Saving subtitles to '%s'" % (local_tmp_file))
         if not os.path.exists(__temp__):
@@ -337,6 +345,7 @@ if params['action'] == 'search':
   item['title']              = normalizeString(xbmc.getInfoLabel("VideoPlayer.OriginalTitle")) # try to get original title
   item['file_original_path'] = urllib.unquote(xbmc.Player().getPlayingFile().decode('utf-8'))  # Full path of a playing file
   item['3let_language']      = []
+  #log("Divxplanet: %s" % xbmc.Player().getVideoInfoTag().getIMDBNumber())
 
   for lang in urllib.unquote(params['languages']).decode('utf-8').split(","):
     item['3let_language'].append(xbmc.convertLanguage(lang,xbmc.ISO_639_2))
